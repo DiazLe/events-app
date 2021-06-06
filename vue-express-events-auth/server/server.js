eb0f96d601f41368565e066f3b44e6065f5e9a3b
Leonor Diaz <diazleonor84@gmail.com>
2021-06-05 23:19:14 -0700

    update

--- vue-express-events-auth/server/server.js
+++ vue-express-events-auth/server/server.js
@@ -18,7 +18,7 @@ let events =
     description: 'One night only! Meet our staff, learn our history and enjoy a nice meal with friends, while helping raise funds for this great organization.',
     location: '555 E. Broadway',
     date: '10-10-2021',
-    time: '5:00'
+    time: '5:00pm'
   },
   {
     id: 2,
@@ -27,7 +27,7 @@ let events =
     description: 'Join us on this night-time adventure. Learn about the different species of squirrels all while playing games, enjoying local food trucks and use loaned telescopes to stargaze.',
     location: 'Squirrel Valley Lodge',
     date: '12-15-2021',
-    time: '7:00'
+    time: '7:00pm'
   }
 ];
 
